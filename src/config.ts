9df1627b98e2027fd66893edbc05f8600937f00e
nukeop <12746779+nukeop@users.noreply.github.com>
2026-04-08 00:52:51 +0200

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const METADATA_PROVIDER_ID = 'khinsider';
+export const STREAMING_PROVIDER_ID = 'khinsider-stream';
+export const BASE_URL = 'https://downloads.khinsider.com';
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import type { NuclearPlugin } from '@nuclearplayer/plugin-sdk';
+
+const plugin: NuclearPlugin = {
+  onEnable() {},
+  onDisable() {},
+};
+
+export default plugin;
